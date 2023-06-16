99cf1d3cc266c2c36cec71aa7861b331c07e4d86
Elisandro Santolin <elisandro@MacBook-Air-de-Elisandro.local>
2023-06-15 21:40:33 -0300

    Feat: added class exercises

---
+++ s01Exe/exercicio1.py
@@ -0,0 +1,1 @@
+print('Elisandro Luiz Santolin')
---
+++ s01Exe/exercicio4.py
@@ -0,0 +1,10 @@
+# Dada as duas váriaveis abaixo:
+
+ABC = 12
+
+abc = '12'
+
+# Podemos afirmar que as duas variáveis são iguais? Justifique sua resposta.
+
+#R: As duas váriaveis são diferentes pois, uma se trata de um valor inteiro e a outra é uma string. 
+#   Além das váriaveis terem nomes diferentes. O python é case sensitive.
