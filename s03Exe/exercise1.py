12aeb61fdaa1321112bcc575501fb5e54d4a4ac2
Elisandro Santolin <elisandro.santolin@outlook.com>
2023-07-05 19:51:32 -0300

    Refactor: correction semantic, not finalized

--- s03Exe/exercise1.py
+++ s03Exe/exercise1.py
@@ -1,10 +1,11 @@
 # 1. Adicione os números de 1 a 10 à lista "minha_lista" utilizando um loop, em seguida imprima esses valores na tela e por fim
 # mostre o tamanho dessa lista.
 
-my_list = 1
+my_list = []
 counter = 1
 
 while(counter <= 10):
     print(my_list)
     counter += 1
     my_list += 1
+
--- s03Exe/exercise12.py
+++ s03Exe/exercise12.py
@@ -1,2 +1,3 @@
-# 12. Faça um programa que recebe um número inteiro do usuário e imprime na tela a quantidade 
-# de divisores desse número e quais são eles.
+# 12. Faça um programa que recebe um número inteiro do usuário e imprime na tela a quantidade de divisores desse número e quais são eles.
+
+number = (input('Digite um número: '))
